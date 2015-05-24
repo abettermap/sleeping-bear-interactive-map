8c2fdbdd010fd20d41fe56b3e5a59b55e8768909
Jason Lampel <travelampel@gmail.com>
2015-05-24 18:41:43 -0400

    index.php cleanup (consolidate SVG/PHP includes)

--- index.php
+++ index.php
@@ -62,15 +62,15 @@
         <script type="text/javascript" src="src/assets/js/vendor/fastclick.js" async></script>
         <script type="text/javascript">
 
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-41881011-18']);
-          _gaq.push(['_trackPageview']);
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-41881011-18']);
+            _gaq.push(['_trackPageview']);
 
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
+            (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
 
         </script>
     </head>
@@ -93,58 +93,28 @@
              fjs.parentNode.insertBefore(js, fjs);
            }(document, 'script', 'facebook-jssdk'));
         </script>
-        <span class="never-show">
-          <?php include('src/assets/img/svg/project/defs/fosb-svg-defs.svg'); ?>
-        </span>
-        <span id="mss-trail_condition" class="never-show">
-            <?php
-              $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-trail_condition.mss');
-              echo $contents;
-            ?>
-        </span>
-        <span id="mss-features" class="never-show">
-            <?php
-              $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-features.mss');
-              echo $contents;
-            ?>
-        </span>
-        <span id="mss-commercial" class="never-show">
-            <?php
-              $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-commercial.mss');
-              echo $contents;
-            ?>
-        </span>
-        <span id="mss-lines" class="never-show">
-            <?php
-              $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-trail.mss');
-              echo $contents;
-            ?>
-        </span>
-        <div class="never-show" ng-include="src/assets/img/svg/project/defs/fosb-svg-defs.svg"></div>
-        <svg class="never-show" xmlns="http://www.w3.org/2000/svg" version="1.1">
-            <defs>
-              <filter id="shadowed-goo">
-                  <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
-                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-                  <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
-                  <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
-                  <feOffset in="shadow" dx="1" dy="1" result="shadow" />
-                  <feComposite in2="shadow" in="goo" result="goo" />
-                  <feComposite in2="goo" in="SourceGraphic" result="mix" />
-              </filter>
-              <filter id="goo">
-                  <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
-                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-                  <feComposite in2="goo" in="SourceGraphic" result="mix" />
-              </filter>
-            </defs>
-        </svg>
         <div id="map-wrapper" class="map-wrapper">
           <interactive-map></interactive-map>
           <panels></panels>
           <map-controls></map-controls>
           <div ui-view></div>
         </div>
+        <?php
+            $contents = file_get_contents( 'src/assets/img/svg/project/defs/fosb-svg-defs.svg' );
+            echo '<span class="never-show">' . $contents . '</span>';
+            $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-trail_condition.mss');
+            echo '<span id="mss-trail_condition" class="never-show">' . $contents . '</span>';
+            $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-features.mss');
+            echo '<span id="mss-features" class="never-show">' . $contents . '</span>';
+            $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-commercial.mss');
+            echo '<span id="mss-commercial" class="never-show">' . $contents . '</span>';
+            $contents = file_get_contents( 'src/assets/spatial/carto-css/sbht-trail.mss');
+            echo '<span id="mss-lines" class="never-show">' . $contents . '</span>';
+            $contents = file_get_contents( 'src/assets/img/svg/project/defs/fosb-svg-defs.svg');
+            echo '<span class="never-show">' . $contents . '</span>';
+            $contents = file_get_contents( 'src/assets/img/svg/project/defs/gooey.svg');
+            echo '<span class="never-show">' . $contents . '</span>';
+        ?>
         <script type="text/javascript" src="//cartodb-libs.global.ssl.fastly.net/cartodb.js/v3/3.12/cartodb.js"></script>
 
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script>
